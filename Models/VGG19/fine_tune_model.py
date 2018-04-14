08889329b089bc586d2605274a079e4e1dc4fce5
Rasmus Ørndrup <rasmus_oerndrup@hotmail.com>
2018-04-14 18:01:07 +0200

    Added DataGenerators to the VGG/fine_tune_model.py and changed how we define which layers to retrain/fine tune

--- Models/VGG19/fine_tune_model.py
+++ Models/VGG19/fine_tune_model.py
@@ -7,12 +7,13 @@ import argparse
 from keras.models import load_model
 from keras import optimizers
 from keras.callbacks import LearningRateScheduler
+from Tools.DataGenerator import DataGenerator
 
 
-def fine_tune_model(train_data, train_lbl, val_data, val_lbl, clf_path, model_path, output_dir, freeze_layers, max_epochs, init_lr, batch_size, lr_sched=None):
-    # Load data
-    training_data, training_labels = image_reader(training_data_path,training_lbls_path)
-    validation_data, validation_labels = image_reader(validation_data_path,validation_lbls_path)
+def fine_tune_model(train_data, train_lbl, val_data, val_lbl, clf_path, model_path, output_dir, retrain_layer_name, max_epochs, init_lr, batch_size, lr_sched=None):
+    # Load labels
+    training_labels = load_labels(train_lbl)
+    validation_labels = load_labels(val_lbl)
     
     # labels must be from 0-num_classes-1, so label offset is subtracted
     label_offset = int(unique[0])
@@ -27,8 +28,17 @@ def fine_tune_model(train_data, train_lbl, val_data, val_lbl, clf_path, model_pa
     model = load_model(model_path)
 
     # freeze the specified layers of the pre-trained model
-    for layer in model.layers[:freeze_layers]
-        layer.trainable = False
+    if retrain_layer_name.isdigit():
+        for layer in model.layers[:-int(retrain_layer_name)]:
+            layer.trainable = False
+    elif retrain_layer_name.lower() == 'all':
+        for layer in model.layers:
+            layer.trainable = False
+    else:
+        for layer in model.layers:
+            if layer.name == retrain_layer_name:
+                break
+            layer.trainable = False
 
     # load classifier and add it on top of pre-trained model
     clf = load_model(clf_path)
@@ -42,21 +52,30 @@ def fine_tune_model(train_data, train_lbl, val_data, val_lbl, clf_path, model_pa
     early = EarlyStopping(monitor='val_acc', min_delta=0, patience=10, verbose=1, mode='auto')
     tb_path = os.path.join(output_dir,'Graph')
     tensorboard = TensorBoard(log_dir=tb_path, histogram_freq=0, write_graph=True, write_images=True, write_grads=True)
+    
     if not lr_sched is None:
         lrate = LearningRateScheduler(step_decay(init_lr, lr_sched[0], lr_sched[1]))
         callback_list = [checkpoint, early, tensorboard, lrate]
     else:
         callback_list = [checkpoint, early, tensorboard]
 
-    # TODO: Data generators
+    # Data generators
+    train_generator = DataGenerator(path_to_images=train_data,
+                                    labels=cat_train_labels, 
+                                    batch_size=batch_size)
+    val_generator = DataGenerator(  path_to_images=val_data,
+                                    labels=cat_val_labels, 
+                                    batch_size=batch_size)
 
     # fit model
     model.fit_generator(train_generator,
-                        steps_per_epoch = len(train_data) / batch_size,
+                        steps_per_epoch = len(train_data)/batch_size,
                         epochs = max_epochs,
                         validation_data = validation_generator,
                         validation_steps = len(val_data)/batch_size,
-                        callbacks = callback_list)
+                        callbacks = callback_list,
+                        workers=4,
+                        use_multiprocessing=True)
 
     # save final model
     model.save_weights('final_model.h5')
@@ -67,13 +86,13 @@ if __name__ == "__main__":
                                     description='''Train last layers of pretrained convolutional neural network and store the output to the specified directory''')
 
     parser.add_argument('train_data', 
-                        help='path to training data vector', nargs='?', default='../../Data/Train/trainVectors.txt')
+                        help='path to directory containing training images', nargs='?', default='../../Data/Train/TrainImages')
 
     parser.add_argument('train_label', 
                         help='path to training label vector', nargs='?', default='../../Data/Train/trainLbls.txt')
 
     parser.add_argument('val_data', 
-                        help='path to validation data vector', nargs='?', default='../../Data/Validation/valVectors.txt')
+                        help='path to directory containing validation images', nargs='?', default='../../Data/Validation/ValidationImages')
 
     parser.add_argument('val_label', 
                         help='path to validation label vector', nargs='?', default='../../Data/Validation/valLbls.txt')
@@ -87,10 +106,9 @@ if __name__ == "__main__":
     parser.add_argument('output', 
                         help='output directory where results are stored')
     
-    parser.add_argument('-freeze_layers', 
-                        help='Number of layers in pre-trained model to freeze during fine-tuning',
-                        type=int,
-                        default='SGD')
+    parser.add_argument('-retrain_layer_name', 
+                        help='The layer name from where the model should be fine-tuned, \"all\" is a valid value, where all layers will be frozen except the classifier. If a number is given the last \"x\" number of the network will be retrained."
+                        default='All')
 
     parser.add_argument('-epochs', 
                         help='Max number of epochs to run',
